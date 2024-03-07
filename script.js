a8809ec89c0b78a0767890f3e7f9b0e51ea59e59
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-07 21:35:34 +0530

    Add condition to run the game for 5 times

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 alert("Welcome To The 'FAMOUS' Rock Paper Scissors Game");
 
-let  playerSelection = prompt("Wisely pick one of these Rock, Paper or Scissors?", "");
+let playerScore = 0;
+let computerScore = 0;
+let tie = 0;
 
-if(playerSelection){
-playerSelection = playerSelection.charAt(0).toLocaleUpperCase()+playerSelection.slice(1).toLocaleLowerCase();
-}
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if(playerSelection && computerSelection){
     if( (playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection ==="Scissors" && computerSelection === "Paper")){
+        playerScore++;
         return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. You win!`
     }
     else if((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Paper" && playerSelection === "Rock") || (computerSelection ==="Scissors" && playerSelection === "Paper")){
+        computerScore++;
         return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`
     }
     else if( computerSelection === playerSelection){
+        tie++;
         return `It's a Tie! Play Again..`
     }
-    else{
-        return `Please enter only in(Rock, Paper, Scissors).`;
-    }
-    }
-    else{
-        return 'You have cancelled the game!'
     }
 }
   
@@ -41,7 +37,40 @@ function getComputerChoice(){
     }
 
 }
-const computerSelection = getComputerChoice();
-alert(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-  
+
+
+function playGame(){
+    for (let round = 1; round <= 5; round++) {
+        let  playerSelection = prompt("Wisely pick one of these Rock, Paper or Scissors?", "");
+        
+        if (playerSelection !== null) {
+
+            if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+                playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+            } else {
+                alert('Please enter only in (Rock, Paper, Scissors).');
+                return;
+            }
+        } else {
+            alert('You have cancelled the round!');
+            return;
+        }
+
+
+        const computerSelection = getComputerChoice();
+
+        alert(playRound(playerSelection, computerSelection));
+    }
+    //Report the scores
+    if (playerScore > computerScore) {
+        alert(`You win the game! Your score: ${playerScore}, Computer's score: ${computerScore}, Tie: ${tie}`);
+    } else if (playerScore < computerScore) {
+        alert(`Computer wins the game! Your score: ${playerScore}, Computer's score: ${computerScore}, Tie: ${tie}`);
+    } else {
+        alert(`It's a tie! Your score: ${playerScore}, Computer's score: ${computerScore}`);
+    }
+}
+
+
+//Start the game
+playGame();
