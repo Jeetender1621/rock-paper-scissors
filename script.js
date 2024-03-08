ea8495d13b66f8c0550f0966a2b59b19c8ed8ed7
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-08 18:07:57 +0530

    Restructuring the code based on the ask

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
-alert("Welcome To The 'FAMOUS' Rock Paper Scissors Game");
-
-let playerScore = 0;
-let computerScore = 0;
-let tie = 0;
+alert('Welcome to the game of "Rock, Paper, Scissors"');
 
 
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-    if(playerSelection && computerSelection){
-    if( (playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection ==="Scissors" && computerSelection === "Paper")){
-        playerScore++;
-        return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. You win!`
+const getComputerChoice = function () {
+    let randomGen = Math.floor(Math.random() * 3) + 1;
+
+    if (randomGen === 1) {
+        return "Rock";
     }
-    else if((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Paper" && playerSelection === "Rock") || (computerSelection ==="Scissors" && playerSelection === "Paper")){
-        computerScore++;
-        return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`
+    else if (randomGen === 2) {
+        return "Paper";
     }
-    else if( computerSelection === playerSelection){
-        tie++;
-        return `It's a Tie! Play Again..`
+    else if (randomGen === 3) {
+        return "Scissors";
     }
-    else{
-        return `Please Enter in(Rock, Paper, Scissors)!`
-    }
+    else {
+        return `Invalid Computer Selection`
     }
 }
-  
-function getComputerChoice(){
-    const number = Math.floor((Math.random()*3)) +1;
-
-    if(number === 1){
-        return "Rock"
-    }
-    else if(number === 2){
-        return "Paper"
-    }
-    else{
-        return "Scissors"
-    }
-
-}
 
 
-function playGame(){
-    for (let round = 1; round <= 5; round++) {
-        let  playerSelection = prompt("Wisely pick one of these Rock, Paper or Scissors?", "");
-        
-        if (playerSelection !== null) {
 
-            if (playerSelection) {
-                playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLocaleLowerCase();
-            } else {
-                alert('Please enter only in (Rock, Paper, Scissors).');
-                return;
-            }
-        } else {
-            alert('You have cancelled the round!');
-            return;
-        }
+const playGame = function () {
+    let round;
+    let playerScore = 0;
+    let computerScore = 0;
+    let tie = 0;
+    let gamesNotPlayed = 0;
+    for (round = 1; round <= 5; round++) {
 
+        let playerSelection = prompt("Wisely choose between Rock, Paper, Scissors", "Rock");
+        if (playerSelection) playerSelection = playerSelection.charAt(0).toLocaleUpperCase() + playerSelection.slice(1).toLocaleLowerCase();
 
         const computerSelection = getComputerChoice();
-
-        alert(playRound(playerSelection, computerSelection));
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection && computerSelection) {
+                if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+                    playerScore++;
+                    return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. You win!`
+                }
+                else if ((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Paper" && playerSelection === "Rock") || (computerSelection === "Scissors" && playerSelection === "Paper")) {
+                    computerScore++;
+                    return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`
+                }
+                else if (computerSelection === playerSelection) {
+                    tie++;
+                    return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. It's a Tie! Play Again..`
+                }
+                else {
+                    gamesNotPlayed++;
+                    return `Invalid input. Please choose only from (Rock, Paper, Scissors).`
+                }
+            }
+            else if (playerSelection === '') {
+                gamesNotPlayed++;
+                return `Choose any one in (Rock, Paper, Scissors)`;
+            }
+            else if (playerSelection === null) {
+                gamesNotPlayed++;
+                return 'You have cancelled the game!';
+            }
+            else {
+                return 'Game crashed!!!!. Contact System Adminstrator';
+            }
+        }
+        console.log(playRound(playerSelection, computerSelection));
     }
+
     //Report the scores
     if (playerScore > computerScore) {
-        alert(`You win the game! Your score: ${playerScore}, Computer's score: ${computerScore}, Tie: ${tie}`);
+        alert(`Total rounds played: ${--round}. You win the game! Your score: ${playerScore}, Computer's score: ${computerScore}, Tie: ${tie}, Games cancelled or given Incorrect Input :${gamesNotPlayed}`);
     } else if (playerScore < computerScore) {
-        alert(`Computer wins the game! Your score: ${playerScore}, Computer's score: ${computerScore}, Tie: ${tie}`);
+        alert(`Total rounds played: ${--round}. Computer wins the game! Your score: ${playerScore}, Computer's score: ${computerScore}, Tie: ${tie}, Games cancelled or given Incorrect Input :${gamesNotPlayed}`);
     } else {
-        alert(`It's a tie! Your score: ${playerScore}, Computer's score: ${computerScore}`);
+        alert(`Total rounds played: ${--round}. It's a tie! Your score: ${playerScore}, Computer's score: ${computerScore}, Games cancelled or Given IncorrectInput :${gamesNotPlayed}`);
     }
 }
-
-
 //Start the game
 playGame();
