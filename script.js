9cdd31271cd5b6b2b636e57d1d8681b692ff9673
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-15 17:22:51 +0530

    Add styling and result logic for RPS game

--- script.js
+++ script.js
@@ -6,50 +6,61 @@ const getComputerChoice = function () {
 // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll(".buttons-container button");
 
+//result div
 const res = document.querySelector('.result');
 
-// we use the .forEach method to iterate through each button
+let plrScore = document.querySelector('.player-score');
+let playerScore = plrScore.textContent = 0;
 
-let playerScore = 0;
-let computerScore = 0;
+let compScore = document.querySelector('.computer-score');
+let computerScore = compScore.textContent = 0;
+
 let tie = 0;
-let gamesNotPlayed = 0;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection && computerSelection) {
         if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
             playerScore++;
-            return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. You win!`
+            plrScore.textContent = playerScore;
+            return `You selected ${playerSelection}, Computer selected ${computerSelection}. You win!`
         }
         else if ((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Paper" && playerSelection === "Rock") || (computerSelection === "Scissors" && playerSelection === "Paper")) {
             computerScore++;
-            return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`
+            compScore.textContent = computerScore;
+            return `You selected ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`
         }
         else if (computerSelection === playerSelection) {
             tie++;
-            return `You have choosen ${playerSelection}, Computer selected ${computerSelection}. It's a Tie! Play Again..`
+            return `You selected ${playerSelection}, Computer selected ${computerSelection}. It's a Tie! Play Again..`
         }
-        else {
-            gamesNotPlayed++;
-            return `Invalid input. Please choose only from (Rock, Paper, Scissors).`
-        }
-    }
-    else if (playerSelection === '') {
-        gamesNotPlayed++;
-        return `Choose any one in (Rock, Paper, Scissors)`;
-    }
-    else if (playerSelection === null) {
-        gamesNotPlayed++;
-        return 'You have cancelled the game!';
     }
     else {
         return 'Game crashed!!!!. Contact System Adminstrator';
     }
 }
 
+const resetGame = function(){
+    playerScore = 0;
+    computerScore = 0;
+    plrScore.textContent = playerScore;
+    compScore.textContent = computerScore;
+}
+
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener("click", () => {
-        res.textContent = playRound(button.textContent, getComputerChoice())
+        res.textContent = playRound(button.textContent, getComputerChoice());
+        if(playerScore === 5){
+            plrScore.textContent = playerScore;
+            compScore.textContent = computerScore;
+            res.textContent = `You won the game with ${playerScore} points!!!`
+            resetGame();
+        }
+        if(computerScore === 5){
+            plrScore.textContent = playerScore;
+            compScore.textContent = computerScore;
+            res.textContent = `You lost the game with ${computerScore- playerScore} points difference, Computer won the game... Play Again!!!`
+            resetGame();
+        }
     });
 });
