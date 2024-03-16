d458a424e2c6ce4452f125f41e815a85ea4f5c10
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-16 15:49:52 +0530

    Update UI with emoji buttons and Logic for rps-v3

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
 const getComputerChoice = function () {
-    let computerChoice = ["Rock", "Paper", "Scissors"]
+    let computerChoice = ["Rock", "Paper", "Scissors"];
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
-}
+};
 
 // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll(".buttons-container button");
 
-//result div
+// Result div
 const res = document.querySelector('.result');
 
-let plrScore = document.querySelector('.player-score');
-let playerScore = plrScore.textContent = 0;
-
-let compScore = document.querySelector('.computer-score');
-let computerScore = compScore.textContent = 0;
+let playerScore = document.querySelector('.player-score');
+let computerScore = document.querySelector('.computer-score');
 
 let tie = 0;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection && computerSelection) {
         if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-            playerScore++;
-            plrScore.textContent = playerScore;
-            return `You selected ${playerSelection}, Computer selected ${computerSelection}. You win!`
+            parseInt(playerScore.textContent++);
+            return `You selected ${playerSelection}, Computer selected ${computerSelection}. You win!`;
         }
         else if ((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Paper" && playerSelection === "Rock") || (computerSelection === "Scissors" && playerSelection === "Paper")) {
-            computerScore++;
-            compScore.textContent = computerScore;
-            return `You selected ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`
+            parseInt(computerScore.textContent++);
+            return `You selected ${playerSelection}, Computer selected ${computerSelection}. Computer wins!`;
         }
         else if (computerSelection === playerSelection) {
-            tie++;
-            return `You selected ${playerSelection}, Computer selected ${computerSelection}. It's a Tie! Play Again..`
+            return `You selected ${playerSelection}, Computer selected ${computerSelection}. It's a Tie! Play Again..`;
         }
     }
     else {
@@ -39,28 +33,39 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const resetGame = function(){
-    playerScore = 0;
-    computerScore = 0;
-    plrScore.textContent = playerScore;
-    compScore.textContent = computerScore;
+
+function resetGame() {
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    res.style.color = 'black';
 }
 
+let shouldResetScores = false; // Flag to indicate if scores should be reset
+
 buttons.forEach((button) => {
-    // and for each one we add a 'click' listener
+    // For each button, add a 'click' listener
     button.addEventListener("click", () => {
-        res.textContent = playRound(button.textContent, getComputerChoice());
-        if(playerScore === 5){
-            plrScore.textContent = playerScore;
-            compScore.textContent = computerScore;
-            res.textContent = `You won the game with ${playerScore} points!!!`
+        if (shouldResetScores) {
             resetGame();
+            shouldResetScores = false; // Reset the flag
         }
-        if(computerScore === 5){
-            plrScore.textContent = playerScore;
-            compScore.textContent = computerScore;
-            res.textContent = `You lost the game with ${computerScore- playerScore} points difference, Computer won the game... Play Again!!!`
-            resetGame();
+
+        // Determine the result of the round
+        const resultMessage = playRound((button.id.charAt(0).toLocaleUpperCase() + button.id.slice(1).toLocaleLowerCase()), getComputerChoice());
+
+        // Update result message
+        res.textContent = resultMessage;
+
+        // Check if either player or computer has reached 5 points
+        if (parseInt(playerScore.textContent) === 5) {
+            res.textContent = `You WON the game... Play Again!!!`;
+            res.style.color = 'green';
+            shouldResetScores = true; // Set the flag to reset scores on next round
+        }
+        if (parseInt(computerScore.textContent) === 5) {
+            res.textContent = `You LOST, Computer won the game... Play Again!!!`;
+            res.style.color = 'red';
+            shouldResetScores = true; // Set the flag to reset scores on next round
         }
     });
 });
